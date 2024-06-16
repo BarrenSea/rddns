ae3688ca391c438680b0db7c83cdc737aa7a0fd6
donjuanplatinum <donjuan@lecturify.net>
2024-06-16 11:33:15 -0400

    添加nsd的匹配域名功能

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use clap::Parser;
 use rddns::{Config, Server};
+use std::sync::Arc;
 #[tokio::main]
 async fn main() {
-    let server: Server = Server::new(Config::parse());
+    let config = Arc::new(Config::parse());
+    let server: Server = Server::new(config);
+    println!("{:?}", server);
     server.run().await;
 }
--- src/server.rs
+++ src/server.rs
@@ -1,32 +1,43 @@
 use crate::routers;
 use crate::Config;
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpServer};
 use owo_colors::OwoColorize;
+use std::sync::Arc;
 #[derive(Debug)]
 pub struct Server {
-    config: Config,
+    config: Arc<Config>,
 }
 
 impl Server {
-    pub fn new(config: Config) -> Self {
+    pub fn new(config: Arc<Config>) -> Self {
         return Server { config };
     }
     pub async fn run(&self) -> () {
-        println!(
-            "Server Running On {} with {} workers use {} as nsd zone file",
-            format!("{}", self.config.address().yellow()),
-            format!("{}", self.config.workers().blue()),
-            format!("{}", self.config.path().purple())
-        );
-        HttpServer::new(|| App::new().service(routers::get_ip).service(routers::ddns))
-            .bind(&self.config.address())
-            .expect(&format!("{}", "Unable To Bind Server !".red()))
-            .workers(self.config.workers())
-            .run()
-            .await
-            .expect(&format!("{}", "Unable To Run the Server !".red()));
+        let state = web::Data::new(State::new(self.config.clone()));
+        HttpServer::new(move || {
+            App::new()
+                .app_data(state.clone())
+                .service(routers::get_ip)
+                .service(routers::ddns)
+        })
+        .bind(self.config.address())
+        .expect(&format!("{}", "Unable To Bind Server !".red()))
+        .workers(self.config.workers())
+        .run()
+        .await
+        .expect(&format!("{}", "Unable To Run the Server !".red()));
     }
-    pub fn config(&self) -> &Config {
-        return &self.config;
+}
+
+pub struct State {
+    config: Arc<Config>,
+}
+
+impl State {
+    pub fn new(config: Arc<Config>) -> State {
+        return State { config };
+    }
+    pub fn config(&self) -> Arc<Config> {
+        return self.config.clone();
     }
 }
