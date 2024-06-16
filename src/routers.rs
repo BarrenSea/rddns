834e43009fd99eabd191315a1bce6129d5649e34
donjuanplatinum <donjuan@lecturify.net>
2024-06-16 01:00:27 -0400

    增加/ddns/domain/ip接口

--- src/routers.rs
+++ src/routers.rs
@@ -1,4 +1,6 @@
-use actix_web::{get, post, HttpRequest, Responder};
+// use super::nsd::*;
+
+use actix_web::{get, post, web, HttpRequest, Responder};
 
 use owo_colors::OwoColorize;
 
@@ -10,6 +12,20 @@ pub async fn get_ip(req: HttpRequest) -> impl Responder {
 }
 
 #[post("/ddns/{domain}/{address}")]
-pub async fn ddns(req: HttpRequest, domain: String, address: String) -> impl Responder {
-    return format!("{}\n", req.peer_addr().unwrap().ip());
+pub async fn ddns(req: HttpRequest, path: web::Path<(String, String)>) -> impl Responder {
+    let peer_addr = req
+        .peer_addr()
+        .expect("Unable To Get The Address Of Peer Address");
+    println!(
+        "[DDNS Request] {} Request {} for {}\n",
+        format!("{}", peer_addr.purple()),
+        format!("{}", path.0).blue(),
+        format!("{}", path.1).green()
+    );
+    return format!(
+        "{} Request {} for {}\n",
+        format!("{}", peer_addr.ip()).yellow(),
+        format!("{}", path.0).blue(),
+        format!("{}", path.1).green()
+    );
 }
