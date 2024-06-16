0bc360e4cb26c533290fb4a5c1337002b03c4d4d
donjuanplatinum <donjuan@lecturify.net>
2024-06-16 19:32:44 -0400

    完善nsd的写入功能

--- src/nsd.rs
+++ src/nsd.rs
@@ -1,33 +1,83 @@
-//use std::io::{self, BufReader};
-// use std::fs::File;
+//use tokio::fs::OpenOptions;
 use super::server::State;
 use actix_web::web;
 use owo_colors::OwoColorize;
-use tokio::fs::File;
-use tokio::io;
-use tokio::io::AsyncReadExt;
+use std::fs::File;
+use std::io::Read;
+use std::io::{self};
+//use tokio::fs::File;
+//use tokio::io::{self, AsyncWriteExt};
+//use tokio::io::AsyncReadExt;
 
 pub async fn read_from_nsd(data: web::Data<State>, domain: &str, address: &str) -> io::Result<()> {
-    let mut file = File::open((**data).config().path()).await?;
+    //    println!("将要从{}读取",(**data).config().path());
+    let mut file = File::open((**data).config().path()).expect(&format!(
+        "{}{}\n",
+        "Can not Open ".red(),
+        (**data).config().path().red()
+    ));
+    //    let mut file = OpenOptions::new().write(true).open((**data).config().path()).await?;
     let mut content = String::new();
-    file.read_to_string(&mut content).await?;
+    file.read_to_string(&mut content).expect(&format!(
+        "{}{}\n",
+        "Can not Read From ".red(),
+        (**data).config().path().red()
+    ));
+    //    println!("原始文件{}\n",content);
     let mut is_zone_region = false;
-    for i in content.lines() {
+    let mut new = String::new();
+    // 遍历zone文件每行
+    for line in content.lines() {
+        //	eprintln!("第{}次循环",index);
+        // 跳过其他区域
         if !is_zone_region {
-            if i.contains(")") {
+            if line.contains(")") {
                 is_zone_region = true;
+                new.push_str(&line);
+                new.push('\n');
             } else {
+                new.push_str(&line);
+                new.push('\n');
                 continue;
             }
+        // zone区域
         } else {
-            let mut i = i.split_ascii_whitespace();
-            if i.nth(0).expect("无法正常读取nsd").contains(domain) {
-                println!("[{}]\t{domain}", "匹配到域名".purple());
-                println!("原地址是{}", i.nth(3).expect(""));
-                println!("欲替换地址是{}", address.yellow());
+            // 以空格分割
+            let mut i = line.split_ascii_whitespace();
+            // 匹配到域名
+            let nth0 = i.nth(0).expect(&format!(
+                "{}{}\n",
+                "Can not Read From ".red(),
+                (**data).config().path().red()
+            ));
+            if nth0 == domain {
+                println!("{} {}", "Find ".green(), domain.purple());
+                new.push_str(domain);
+                new.push('\t');
+                new.push_str(&i.next().unwrap());
+                new.push('\t');
+                new.push_str(&i.next().unwrap());
+                new.push('\t');
+                new.push_str(&i.next().unwrap());
+                new.push('\t');
+                new.push_str(address);
+                new.push('\n');
+            }
+            // 未匹配到域名
+            else {
+                new.push_str(&line);
+                new.push('\n');
             }
         }
     }
-    //    println!("{}",content);
+    //    println!("新字符串{}",&new);
+    std::fs::write((**data).config().path(), new).expect(&format!(
+        "{}{}\n",
+        "Can not Write To ".red(),
+        (**data).config().path().red()
+    ));
+    //    file.write(new.as_bytes()).expect("写入失败");
+    //    println!("写入完成");
+    println!("{}", "Succuess To Write".purple());
     return Ok(());
 }
